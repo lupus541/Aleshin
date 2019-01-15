e2ae66435b9c20b78c5426c540df1e637c524f4b
lupus541 <38623811+lupus541@users.noreply.github.com>
2019-01-15 12:48:40 +0300

    Aleshin ( YBVT  -  1861 )

---
+++ Main.java
@@ -0,0 +1,26 @@
+public class Main {
+ 
+ 
+    public static void main(String[] args) {
+ 
+		// 1 Question. //
+		
+			System.out.println("Hello world?");
+		
+		// 2 Question. //
+			
+			System.out.println(isPalindrome("Здоровеньки"));
+		
+			System.out.println(isPalindrome("шабаш"));
+        
+		// 3 Question. //
+        
+			for (int i = 0; i < 100; i++) {
+        
+            if (isPrime(i))
+                System.out.println(i);
+        }
+    }
+ 
+
+}
